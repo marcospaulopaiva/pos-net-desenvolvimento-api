2e69fb4869cc2196b46f39a1155226e588619460
Marcos Paulo <marcos.poo@gmail.com>
2024-11-18 17:59:18 -0300

    feat: Criação do projeto de Testes.

---
+++ PlaceRentalApp/PlaceRentalApp.UnitTests/Core/PlaceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaceRentalApp.UnitTests.Core
+{
+    public class PlaceTests
+    {
+        [Fact]
+        public void Update_PlaceAndDateAreOk_Success()
+        {
+            // Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
