a73d4468415c1969e1ef43c2e4c38c9373740352
Marcos Paulo <marcos.poo@gmail.com>
2024-11-18 18:29:17 -0300

    feat: Implementando alguns testes.

--- PlaceRentalApp/PlaceRentalApp.UnitTests/Core/PlaceTests.cs
+++ PlaceRentalApp/PlaceRentalApp.UnitTests/Core/PlaceTests.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PlaceRentalApp.Core.ValueObjects;
+using static PlaceRentalApp.Core.Entities.BaseEntity;
 
 namespace PlaceRentalApp.UnitTests.Core
 {
@@ -12,11 +9,117 @@ namespace PlaceRentalApp.UnitTests.Core
         public void Update_PlaceAndDateAreOk_Success()
         {
             // Arrange
+            var place = new Place(
+                "Title",
+                "Test Description",
+                100m,
+                new Address("Street", "123", "12345678","District","City", "State","BR"),
+                4,
+                true,
+                123);
+
+            var newTitle = "New Title";
+            var newDescription = "New Description";
+            var newDailyPrice = 140m;
 
             //Act
+            var result = place.Update(newTitle, newDescription, newDailyPrice);
 
             //Assert
+            Assert.True(result);
+            Assert.Equal(newTitle, place.Title);
+            Assert.Equal(newDescription, place.Description);
+            Assert.Equal(newDailyPrice, place.DailyPrice);
 
+        }
+
+        [Fact]
+        public void Update_PlaceIsBlocked_False()
+        {
+            // Arrange
+            var place = new Place(
+                "Title",
+                "Test Description",
+                100m,
+                new Address("Street", "123", "12345678", "District", "City", "State", "BR"),
+                4,
+                true,
+                123);
+
+            var newTitle = "New Title";
+            var newDescription = "New Description";
+            var newDailyPrice = 140m;
+
+            place.Block();
+
+            //Act
+            var result = place.Update(newTitle, newDescription, newDailyPrice);
+
+            //Assert
+            Assert.False(result);
+            Assert.NotEqual(newTitle, place.Title);
+            Assert.NotEqual(newDescription, place.Description);
+            Assert.NotEqual(newDailyPrice, place.DailyPrice);
+        }
+
+        [Fact]
+        public void IsBookAllowed_AmountOfPersonAndPetOk_Success()
+        {
+            // Arrange
+            var place = new Place(
+                "Title",
+                "Test Description",
+                100m,
+                new Address("Street", "123", "12345678", "District", "City", "State", "BR"),
+                4,
+                true,
+                123);
+
+            // Act
+            var isBookAllowed = place.IsBookAllowed(true, 2);
+
+            // Assert
+            Assert.True(isBookAllowed);
+        }
+
+        [Fact]
+        public void IsBookAllowed_HasPetButNotAllowed_False()
+        {
+            // Arrange
+            var place = new Place(
+               "Title",
+               "Test Description",
+               100m,
+               new Address("Street", "123", "12345678", "District", "City", "State", "BR"),
+               4,
+               false,
+               123);
+
+            // Act
+            var isBookAllowed = place.IsBookAllowed(true, 3);
+
+            // Assert
+            Assert.False(isBookAllowed);
+        }
+
+        [Fact]
+        public void IsBookAllowed_OverMaximumAllowedPerson_False()
+        {
+            // Arrange
+            var place = new Place(
+               "Title",
+               "Test Description",
+               100m,
+               new Address("Street", "123", "12345678", "District", "City", "State", "BR"),
+               4,
+               true,
+               123);
+
+            // Act
+            var isBookAllowed = place.IsBookAllowed(true, 5);
+
+            // Assert
+            Assert.False(isBookAllowed);
         }
     }
 }
